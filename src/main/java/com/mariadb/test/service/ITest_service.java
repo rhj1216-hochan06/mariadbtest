d12550a66b717d39f44b6016d2b69e2a2754e8d0
ryuhojin <ryuhojin@ryuhojinui-MacBookPro.local>
2023-07-10 17:27:53 +0900

    crud완성(postMapping)

--- src/main/java/com/mariadb/test/service/ITest_service.java
+++ src/main/java/com/mariadb/test/service/ITest_service.java
@@ -3,9 +3,10 @@ import com.mariadb.test.model.Test_model;
 import java.util.List;
 
 public interface ITest_service {
-    List<Test_model> TestSelectTest(Test_model param);
-    List<Test_model> TestSelectTestAll(Test_model param);
-    List<Test_model> TestSelect();
-
+    void TestInsert(Test_model param);
+    Test_model TestSelect(String pid);
+    void TestUpdate(Test_model param);
+    void TestDelete(String pid);
+    List<Test_model> TestSelectSearch(Test_model param);
     String Time();
 }
--- src/main/java/com/mariadb/test/service/impl/Test_service.java
+++ src/main/java/com/mariadb/test/service/impl/Test_service.java
@@ -1,35 +1,47 @@
+// Test_service.java
+
 package com.mariadb.test.service.impl;
 
 import com.mariadb.test.mapper.Test_mapper;
 import com.mariadb.test.model.Test_model;
 import com.mariadb.test.service.ITest_service;
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class Test_service implements ITest_service {
+
     @Autowired
     Test_mapper test_mapper;
+
     @Override
-    public List<Test_model> TestSelectTest(Test_model param){
-        return test_mapper.TestSelectTest(param);
-    }
-    @Override
-    public List<Test_model> TestSelectTestAll(Test_model param){
-        return test_mapper.TestSelectTestAll(param);
+    public void TestInsert(Test_model param) {
+        test_mapper.TestInsert(param);
     }
 
     @Override
-    public List<Test_model> TestSelect(){
-        System.out.println("\n 여기까지 오는지 확인2 \n");
-        return test_mapper.TestSelect();
+    public Test_model TestSelect(String pid) {
+        return test_mapper.TestSelect(pid);
+    }
+
+    @Override
+    public void TestUpdate(Test_model param) {
+        test_mapper.TestUpdate(param);
+    }
+
+    @Override
+    public void TestDelete(String pid) {
+        test_mapper.TestDelete(pid);
+    }
+
+    @Override
+    public List<Test_model> TestSelectSearch(Test_model param) {
+        return test_mapper.TestSelectSearch(param);
     }
     @Override
-    public String Time(){
-        System.out.println("\n 여기까지 오는지 확인2 \n");
+    public String Time() {
         return test_mapper.Time();
     }
-
 }
